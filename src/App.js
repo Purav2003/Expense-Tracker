5f33f49bc7d91ae96a873131366b9ba269c11a93
Purav <puravshah20@gnu.ac.in>
2023-07-15 16:17:22 +0530

    Updated Structure

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import Form from './Form';
+function App() {
+  return (
+    <Form />
+  );
+}
+
+export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,26 @@
+import axios from "axios"
+const Form = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  const username = document.querySelector('.username').value
+  const password = document.querySelector('.password').value
+
+  try {
+    await axios.post('http://localhost:3001/api/v1/expresstracker', { username,password })   
+  } catch (error) {
+    console.log(error)
+  }
+}
+  return (
+
+
+    <form onSubmit={handleSubmit}>
+      Name:<input type="text" name="userame" className="username" ></input><br></br>
+      Password:<input type="password" name="password" className="password"></input><br></br>
+      <button type="submit" >Submit</button>
+    </form>
+  )
+}
+export default Form
+
