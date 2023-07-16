2f7a43525723c43ee8826b25ddef0063195899f3
Purav <puravshah20@gnu.ac.in>
2023-07-16 20:02:14 +0530

    Updated signup

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import signup from '../Assets/images/signup.png';
 import "../Assets/css/signup.css";
+import * as icons from 'react-icons/fc';
 const Signup = () => {
 
   const handleSubmit = async (e) => {
@@ -15,18 +16,19 @@ const Signup = () => {
       await axios.post('http://localhost:5000/api/v1/auth/signup', { email, password, name, mobile})
     } catch (error) {
       
-    }</>:<div>
-      <h1 style={{background:'yellow',textAlign:'center'}}>Password to sarkho nakh be</h1>    
+    }</>:console.log("Error")
+    document.getElementById('error').innerHTML += '<h1>Invalid Phone Number</h1>';
         
-    </div>
+    
   }
   return (
     <>
     <div className="lg:flex lg:flex-row">
     <div className="pl-[5vw] pt-[3vw] first-div">
+      <div id="error" className="pl-[2.4vw]"></div>
           <div>
+
             <h1 className="text-[35px] heading-signup font-bold pl-[13vw] pt-[1vw] text-[#1BA329]">Sign up</h1>
-            <p className="pl-[2.4vw] pt-[2.5vh] second-signup">Already have an account? <a href="/login" className="underline text-[#1BA329]">Login</a></p>
           </div>
             <div className="w-full max-w-xs">
               <form onSubmit={handleSubmit} className="bg-white rounded w-[40vw] px-8 pt-6 pb-8 mb-4">
@@ -34,33 +36,38 @@ const Signup = () => {
                   <label className="block text-black-700 text-sm font-bold mb-2 ">
                     Name
                   </label>
-                  <input className="name shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                  <input className="name shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Username" />
                 </div>
                 <div className="mb-4 form-field">
                   <label className="block text-black-700 text-sm font-bold mb-2">
                     Phone Number
                   </label>
-                  <input className="mobile shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="mail" placeholder="Username" />
+                  <input className="mobile shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="Phone Number" />
                 </div>
                 <div className="mb-4 form-field">
                   <label className="block text-black-700 text-sm font-bold mb-2">
                     Email
                   </label>
-                  <input className="email shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                  <input className="email shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="E-mail" />
                 </div>
                 <div className="mb-4 form-field">
                   <label className="block text-black-700 text-sm font-bold mb-2">
                     Password
                   </label>
-                  <input className="password shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="password" placeholder="Username" />
+                  <input className="password shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="Password" />                  
                 </div>               
                 <div className="flex form-field items-center">
-                  <button className="bg-[#1BA329] button w-full text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline" type="button">
+                  <button className="bg-[#1BA329] button w-full text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Sign Up
                   </button>
-
+                  <button className="border content-center border-[#1BA329] ml-[1vw] button w-full text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline" type="button">
+                  <icons.FcGoogle className="icon-google"></icons.FcGoogle>
+                  </button>
+                  
                 </div>
               </form>
+              <p className="pl-[2.4vw] second-signup">Already have an account? <a href="/login" className="underline text-[#1BA329]">Login</a></p>
+
             </div>
         </div>
 <div className="photo-signup ml-[23vw] w-full h-[100vh] pt-[6vw] bg-[#1BA329] invisible sm:invisible md:invisible lg:visible xl:visible">
