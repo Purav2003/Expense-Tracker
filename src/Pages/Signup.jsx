ba9198125669b08deaff3365c3cc0aa0867bc791
Purav <puravshah20@gnu.ac.in>
2023-07-16 19:02:33 +0530

    Updated responsiveness of image in signup

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
     <div className="lg:flex lg:flex-row">
     <div className="pl-[5vw] pt-[3vw] first-div">
           <div>
-            <h1 className="text-[35px] heading-signup font-bold pl-[14vw] pt-[1vw] text-[#1BA329]">Sign up</h1>
+            <h1 className="text-[35px] heading-signup font-bold pl-[13vw] pt-[1vw] text-[#1BA329]">Sign up</h1>
             <p className="pl-[2.4vw] pt-[2.5vh] second-signup">Already have an account? <a href="/login" className="underline text-[#1BA329]">Login</a></p>
           </div>
             <div className="w-full max-w-xs">
@@ -40,7 +40,7 @@ const Signup = () => {
                   <label className="block text-black-700 text-sm font-bold mb-2">
                     Phone Number
                   </label>
-                  <input className="mobile shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                  <input className="mobile shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="mail" placeholder="Username" />
                 </div>
                 <div className="mb-4 form-field">
                   <label className="block text-black-700 text-sm font-bold mb-2">
@@ -52,7 +52,7 @@ const Signup = () => {
                   <label className="block text-black-700 text-sm font-bold mb-2">
                     Password
                   </label>
-                  <input className="password shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                  <input className="password shadow appearance-none border border-black rounded w-full py-2 px-3 text-black-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="password" placeholder="Username" />
                 </div>               
                 <div className="flex form-field items-center">
                   <button className="bg-[#1BA329] button w-full text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline" type="button">
@@ -65,7 +65,7 @@ const Signup = () => {
         </div>
 <div className="photo-signup ml-[23vw] w-full h-[100vh] pt-[6vw] bg-[#1BA329] invisible sm:invisible md:invisible lg:visible xl:visible">
   <h1 className="text-center font-bold text-[30px] text-bold text-white">Create an Account</h1>
-      <img src={signup} className="ml-[3vw] mt-[2vw]"></img>
+      <img src={signup} className="pl-[5vw] pt-[3vw]"></img>
 </div>
     </div>
 
