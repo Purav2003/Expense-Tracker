c2a4420c5cefc39fb5bf0c1a1a8fbfb0bffa497e
Purav <puravshah20@gnu.ac.in>
2023-07-16 11:56:48 +0530

    Updated Signup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route path='/login' element={<Login />}></Route>
+        <Route path='/' element={<Login />}></Route>
         <Route path='/signup' element={<Signup />}></Route>
       </Routes>
     </Router>
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -6,26 +6,27 @@ const Signup = () => {
     const email = document.querySelector('.email').value
     const password = document.querySelector('.password').value
     const name = document.querySelector('.name').value
-    const phone = document.querySelector('.phone').value
+    const phone = document.querySelector('.mobile').value
     const repassword = document.querySelector('.repassword').value
-
-    repassword===password?<>
+    const phonearray = Array.from(phone)
+    repassword===password && phonearray.length==10?<>
     try {
       await axios.post('http://localhost:3001/api/v1/auth/signup', { name, email, password ,phone})        
     } catch (error) {
       
-    }</>:<><h1>Password to sarkho nakh be</h1>    
-        {document.querySelector('.repassword').value}
+    }</>:<div>
+      <h1 style={{background:'yellow',textAlign:'center'}}>Password to sarkho nakh be</h1>    
+        {document.querySelector('.repassword').value=''}
     
-    </>
+    </div>
   }
   return (
     <form onSubmit={handleSubmit}>
-      Name:<input type="name" name="name" className="name" ></input><br></br>
-      Email:<input type="email" name="email" className="email" ></input><br></br>
-      Phone:<input type="number" name="mobile" className="mobile" ></input><br></br>
-      Password:<input type="password" name="password" className="password"></input><br></br>
-      Retype-Password:<input type="password" name="repassword" className="repassword"></input><br></br>
+      Name:<input type="name" name="name" className="name" required></input><br></br>
+      Email:<input type="email" name="email" className="email" required></input><br></br>
+      Phone:<input type="number" name="mobile" className="mobile" required></input><br></br>
+      Password:<input type="password" name="password" className="password" required></input><br></br>
+      Retype-Password:<input type="password" name="repassword" className="repassword" required></input><br></br>
       <button type="submit" >Submit</button>
     </form>
   )
