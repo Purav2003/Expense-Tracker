1320bd5c99c86db98289417b6d1b87a2d7bb9d4d
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-07-17 22:47:14 +0530

    conflict

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const {signin,signup} = require("../controller/user")
+const {signin,signup,getAllUser} = require("../controller/user")
 const passport = require("passport")
 require("../middlewares/passport-setup")
 
+router.route("/").get(getAllUser)
 router.route("/signup").post(signup)
 router.route("/signin").post(signin)
 router.route("/google").get(passport.authenticate('google', { scope:[ 'email', 'profile' ] }))
