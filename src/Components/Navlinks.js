85244b5f0f7c8eb8b9147d427c2bad032afe04ac
Purav <puravshah20@gnu.ac.in>
2023-07-18 19:06:19 +0530

    Changed Logic Of sidenav

---
+++ src/Components/Navlinks.js
@@ -0,0 +1,38 @@
+import * as dash from 'react-icons/rx'
+import * as income from 'react-icons/pi'
+import * as expenses from 'react-icons/fa'
+import * as report from 'react-icons/hi'
+import * as profile from 'react-icons/cg'
+export const navLinks = [
+    {
+      id: "dashboard",
+      title: "Dashboard",
+      icon: <dash.RxDashboard></dash.RxDashboard>,
+      iconm:<dash.RxDashboard className='text-white text-[25px]'></dash.RxDashboard>,
+    },
+    {
+      id: "income",
+      title: "Income",
+      icon: <income.PiMoneyLight></income.PiMoneyLight>,
+      iconm:<income.PiMoneyLight className='text-white text-[25px]'></income.PiMoneyLight>,
+
+    },
+    {
+      id: "expense",
+      title: "Expense",
+      icon: <income.PiMoneyLight></income.PiMoneyLight>,
+      iconm:<income.PiMoneyLight className='text-white text-[25px]'></income.PiMoneyLight>,
+    },
+    {
+      id:"report",
+      title:"Report",
+      icon:<report.HiOutlineDocumentReport></report.HiOutlineDocumentReport>,
+      iconm:<report.HiOutlineDocumentReport className='text-white text-[25px]'></report.HiOutlineDocumentReport>,
+    },
+    {
+      id:"profile",
+      title:"Profile",
+      icon:<profile.CgProfile></profile.CgProfile>,
+      iconm:<profile.CgProfile className='text-white text-[25px]'></profile.CgProfile>,
+    }
+  ];
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,10 @@
-import * as dash from 'react-icons/rx'
-import * as income from 'react-icons/pi'
-import * as expenses from 'react-icons/fa'
-import * as report from 'react-icons/hi'
-import * as profile from 'react-icons/cg'
 import "../Assets/css/sidebar.css"
+import { useState } from 'react'
+import { navLinks } from './Navlinks'
+
 const Sidebar = () => {
+    const [active, setActive] = useState(' ');
+
     return (
         <div>
             <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-75 h-screen -translate-x-full sm:translate-x-0" aria-label="Sidebar">
@@ -13,32 +13,16 @@ const Sidebar = () => {
                         <br></br>   <h1 className='font-bold text-[29px] text-white'>Expense Tracker</h1><br></br><hr></hr><br></br>
                     </div>
                     <ul className="space-y-2 font-medium">
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[rgba(255,255,255,0.1)]  group">
-                                <dash.RxDashboard></dash.RxDashboard><span className="ml-3">Dashboard</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[rgba(255,255,255,0.1)]  group">
-                                <income.PiMoneyLight></income.PiMoneyLight><span className="flex-1 ml-3 whitespace-nowrap">Income</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[rgba(255,255,255,0.1)]  group">
-                                <span className="flex-1 ml-3 whitespace-nowrap">Expenses</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[rgba(255,255,255,0.1)]  group">
-
-                                <report.HiOutlineDocumentReport></report.HiOutlineDocumentReport><span className="flex-1 ml-3 whitespace-nowrap">Report</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[rgba(255,255,255,0.1)]  group">
-                                <profile.CgProfile></profile.CgProfile><span className="flex-1 ml-3 whitespace-nowrap">Profile</span>
-                            </a>
-                        </li>
+                        {navLinks.map((link) => (
+                            <li key={link.id} className={`${active === link.title
+                                ? "text-white bg-[rgba(255,255,255,0.1)] rounded-lg"
+                                : "text-secondary"} hover:text-white text-[18px] font-medium`}
+                                onClick={() => setActive(link.title)}>
+                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[rgba(255,255,255,0.1)]  group">
+                                    {link.icon}<span className="ml-3">{link.title}</span>
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </aside>
@@ -47,9 +31,9 @@ const Sidebar = () => {
 
             </div>
             <div className='mobile-nav fixed top-0 w-full h-16'>
-                    <div className="h-full bg-[#27374D]">
-                                <h1 className='font-bold text-white items-center justify-center py-5 px-3'>Expense Tracker</h1>
-                    </div>
+                <div className="h-full bg-[#27374D]">
+                    <h1 className='font-bold text-white items-center justify-center py-5 px-3'>Expense Tracker</h1>
+                </div>
             </div>
             <div className='w-0 sm:w-full fixed
              inset-x-0
@@ -57,25 +41,17 @@ const Sidebar = () => {
 
                 <div className="fixed bottom-0 left-0 z-50 w-full h-16">
                     <div className="grid h-full max-w-lg grid-cols-5 mx-auto font-medium bg-[#27374D]">
-                        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-[rgba(255,255,255,0.1)] group">
-                            <dash.RxDashboard className='text-white text-[25px]'></dash.RxDashboard>
-                            {/* <span className="text-sm text-white dark:text-white-400 group-hover:text-[#409447] dark:group-hover:text-white">Dashboard</span> */}
-                        </button>
-                        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-[rgba(255,255,255,0.1)] group">
-                            <income.PiMoneyLight className='text-white text-[25px]'></income.PiMoneyLight>
-                            {/* <span className="text-sm text-white dark:text-white-400 group-hover:text-[#409447] dark:group-hover:text-white">Income</span> */}
-                        </button>
-                        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-[rgba(255,255,255,0.1)] group">
-                            {/* <span className="text-sm text-white dark:text-white-400 group-hover:text-[#409447] dark:group-hover:text-white">Expense</span> */}
-                        </button>
-                        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-[rgba(255,255,255,0.1)] group">
-                            <report.HiOutlineDocumentReport className='text-white text-[25px]'></report.HiOutlineDocumentReport>
-                            {/* <span className="text-sm text-white dark:text-white-400 group-hover:text-[#409447] dark:group-hover:text-white">Report</span> */}
-                        </button>
-                        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-[rgba(255,255,255,0.1)] group">
-                            <profile.CgProfile className='text-white text-[25px]'></profile.CgProfile>
-                            {/* <span className="text-sm text-white dark:text-white-400 group-hover:text-[#409447] dark:group-hover:text-white">Profile</span> */}
-                        </button>
+
+                        {navLinks.map((link) => (
+                            <button key={link.id} className={`${active === link.title
+                                ? "text-white bg-[rgba(255,255,255,0.1)] "
+                                : "text-secondary"} inline-flex flex-col items-center justify-center px-5 group`}
+                                onClick={() => setActive(link.title)}>
+                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group">
+                                    {link.iconm}                             </a>
+                            </button>
+                        ))}
+
                     </div>
                 </div>
 
