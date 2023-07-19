a1131e54077c1063a7fd100c1a1c9c24c54acf4b
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-07-19 20:25:06 +0530

    changes again changed back json to send

--- server/models/user.js
+++ server/models/user.js
@@ -1,45 +1,44 @@
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 var userSchema = new mongoose.Schema({
-    name
-    :{
-        type:String,
-        required:[true,"Please enter your name"],
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
     },
-    email:{
-        type:String,
-        required:[true,"Please enter you email"],
-        match:[
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please provide valid email"
-        ],  
-        unique:true,
+    email: {
+        type: String,
+        required: [true, "Please enter you email"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please provide valid email"
+        ],
+        unique: true,
     },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-        match:[/^(\+\d{1,3}[- ]?)?\d{10}$/]
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^(\+\d{1,3}[- ]?)?\d{10}$/]
     },
-    password:{
-        type:String,
-        required:true,
-        match:[/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/]
+    password: {
+        type: String,
+        required: true,
+        match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/]
     },
-},{timestamps:true});
+}, { timestamps: true });
 
-userSchema.pre("save",async function(){
+userSchema.pre("save", async function () {
     const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password,salt)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
-userSchema.methods.comparePassword = function(userPassword){
-    return bcrypt.compare(userPassword,this.password)
+userSchema.methods.comparePassword = function (userPassword) {
+    return bcrypt.compare(userPassword, this.password)
 }
 
-userSchema.methods.createJWT = function(){
-    return jwt.sign({userId:this._id,name:this.name},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+userSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
 }
 
 module.exports = mongoose.model('User', userSchema);
