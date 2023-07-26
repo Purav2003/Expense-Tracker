0ce38c04d776f8f5c9f675f634c365c99f103ffb
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-07-26 22:04:29 +0530

    removed email activity

--- server/controller/user.js
+++ server/controller/user.js
@@ -11,29 +11,29 @@ const signup = async (req, res) => {
         const phoneexist = await User.findOne({ mobile: req.body.mobile })
         if (!phoneexist) {
             const user = await User.create(req.body)
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: process.env.EMAIL,
-                    pass: process.env.PASSWORD   
-                }
-            });
+            // var transporter = nodemailer.createTransport({
+            //     service: 'gmail',
+            //     auth: {
+            //         user: process.env.EMAIL,
+            //         pass: process.env.PASSWORD   
+            //     }
+            // });
 
-            var mailOptions = {
-                from: process.env.EMAIL,
-                to: req.body.email,
-                subject: "Welcome to Expense Tracker! Your Registration is Complete.",
-                html: `<b>Congrulations you have successfully registered with Expense Tracker</b><br><p>Dear ${req.body.name}, <br> Congratulations! We are thrilled to inform you that your registration with Expense Tracker has been successfully completed. On behalf of our team, we extend a warm welcome to you as a valued member of our community. <br> Remember, our team is here to support you every step of the way. Should you have any questions or require assistance, please don't hesitate to reach out to us. Our dedicated support team is available via ${process.env.EMAIL}, and we are committed to ensuring you have a seamless experience.<br>Thank you for choosing Expense Tracker as your trusted financial companion. We look forward to helping you gain control over your finances and achieve your financial aspirations. <br><br> <b>Best regards</b></p>`
-            };
+            // var mailOptions = {
+            //     from: process.env.EMAIL,
+            //     to: req.body.email,
+            //     subject: "Welcome to Expense Tracker! Your Registration is Complete.",
+            //     html: `<b>Congrulations you have successfully registered with Expense Tracker</b><br><p>Dear ${req.body.name}, <br> Congratulations! We are thrilled to inform you that your registration with Expense Tracker has been successfully completed. On behalf of our team, we extend a warm welcome to you as a valued member of our community. <br> Remember, our team is here to support you every step of the way. Should you have any questions or require assistance, please don't hesitate to reach out to us. Our dedicated support team is available via ${process.env.EMAIL}, and we are committed to ensuring you have a seamless experience.<br>Thank you for choosing Expense Tracker as your trusted financial companion. We look forward to helping you gain control over your finances and achieve your financial aspirations. <br><br> <b>Best regards</b></p>`
+            // };
 
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    return res.status(500).send({ error ,success:false,status:500})
-                } else {
+            // transporter.sendMail(mailOptions, function (error, info) {
+            //     if (error) {
+            //         return res.status(500).send({ error ,success:false,status:500})
+            //     } else {
                     const token = user.createJWT()
-                res.status(200).send({ user, token, success: true, status: 200 })
-                }
-            });
+                    res.status(200).send({ user, token, success: true, status: 200 })
+            //     }
+            // });
         } else {
             res.status(403).send({ msg: "Phone Number already exists", success: false, status: 403 })
         }
