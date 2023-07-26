d8af852663528354ce0f3c75a320ad95d5a982b9
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-07-26 21:34:11 +0530

    changes in signup controller

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,46 +1,44 @@
 require("express-async-errors")
 const customAPIError = require("../errors/custom-error")
 const User = require("../models/user")
-const nodemailer = require("nodemailer")
+// const nodemailer = require("nodemailer")
 
 const signup = async (req,res)=>{
+    // const user = await User.create(req.body)
+    // res.status(200).send({user,success:true})
     const userexist = await User.findOne({email:req.body.email})
     if(!userexist){
         const phoneexist = await User.findOne({mobile:req.body.mobile})
         if(!phoneexist){
             const user = await User.create(req.body)
-            const config = {
-                service:"gmail",
-                auth:{
-                    user:process.env.EMAIL,
-                    pass:process.env.PASSWORD   
-                }
-            }
+            // const config = {
+    //             service:"gmail",
+    //             auth:{
+    //                 user:process.env.EMAIL,
+    //                 pass:process.env.PASSWORD   
+    //             }
+    //         }
 
-            const transporter =  nodemailer.createTransport(config)
+    //         const transporter =  nodemailer.createTransport(config)
 
-            const msg = {
-                from: process.env.EMAIL, 
-                to: req.body.email,
-                subject: "Welcome to Expense Tracker! Your Registration is Complete.",
-                html: `<b>Congrulations you have successfully registered with Expense Tracker</b><br><p>Dear ${req.body.name}, <br> Congratulations! We are thrilled to inform you that your registration with Expense Tracker has been successfully completed. On behalf of our team, we extend a warm welcome to you as a valued member of our community. <br> Remember, our team is here to support you every step of the way. Should you have any questions or require assistance, please don't hesitate to reach out to us. Our dedicated support team is available via ${process.env.EMAIL}, and we are committed to ensuring you have a seamless experience.<br>Thank you for choosing Expense Tracker as your trusted financial companion. We look forward to helping you gain control over your finances and achieve your financial aspirations. <br><br> <b>Best regards</b></p>`,
-                }
+    //         const msg = {
+    //             from: process.env.EMAIL, 
+    //             to: req.body.email,
+    //             subject: "Welcome to Expense Tracker! Your Registration is Complete.",
+    //             html: `<b>Congrulations you have successfully registered with Expense Tracker</b><br><p>Dear ${req.body.name}, <br> Congratulations! We are thrilled to inform you that your registration with Expense Tracker has been successfully completed. On behalf of our team, we extend a warm welcome to you as a valued member of our community. <br> Remember, our team is here to support you every step of the way. Should you have any questions or require assistance, please don't hesitate to reach out to us. Our dedicated support team is available via ${process.env.EMAIL}, and we are committed to ensuring you have a seamless experience.<br>Thank you for choosing Expense Tracker as your trusted financial companion. We look forward to helping you gain control over your finances and achieve your financial aspirations. <br><br> <b>Best regards</b></p>`,
+    //             }
 
-            transporter.sendMail(msg).then(() => {
+    //         transporter.sendMail(msg).then(() => {
                 const token = user.createJWT()
-                
-                res.status(200).send({user,token,success:true})
-                console.log("success")
-
-            }).catch(error => {
-                return res.status(500).send({ error })
-            })
+                res.status(200).send({user,token,success:true,status:200})
+    //         }).catch(error => {
+    //             return res.status(500).send({ msg:error,success:false,confirm:"Ha huj" })
+    //         })
         } else{
-            res.status(403).send({msg:"Phone Number already exists",success:false})
+            res.status(403).send({msg:"Phone Number already exists",success:false,status:403})
         }
     } else{
-        // console.log("ama jay che?");
-        res.status(403).send({msg:"User already exists",success:false})
+        res.status(403).send({msg:"User already exists",success:false,status:403})
     }
     
 }
