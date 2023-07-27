7d56bc05609fd331ba75d629c0fb5ae6ebb9e95d
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-07-27 18:39:08 +0530

    updated

--- server/controller/income.js
+++ server/controller/income.js
@@ -8,7 +8,11 @@ const addIncome = async (req,res)=>{
 const getSingleIncome = async (req,res)=>{
     const id = req.params.id
     const expenses = await Income.findById(id)
-    res.send({expenses,count:expenses.length,success:true,status:200})
+    if(!expenses){
+        res.send({expenses,count:0,success:true,status:200})
+    } else{
+        res.send({expenses,count:expenses.length,success:true,status:200})
+    }
 }
 
 module.exports = {
