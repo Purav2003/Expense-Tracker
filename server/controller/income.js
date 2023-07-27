4c1e7279d7eb512409cbc3556aa28aa01461611b
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-07-27 18:19:42 +0530

    added the controller for fetching income of a person

--- server/controller/income.js
+++ server/controller/income.js
@@ -5,21 +5,13 @@ const addIncome = async (req,res)=>{
     res.send({income,success:true,status:200})
 }
 
-const getIncome = async (req,res)=>{
-    const expenses = await Income.find()
+const getSingleIncome = async (req,res)=>{
+    const id = req.params.id
+    const expenses = await Income.findById(id)
     res.send({expenses,count:expenses.length,success:true,status:200})
 }
 
-// const deleteExpense = async (req,res)=>{
-//     const income = await Income.findByIdAndDelete(req.params.id)
-//     if(!income){
-//         res.send({msg:"Income not found "})
-//     }
-//     res.status(200).send({msg:"Income deleted successfully",success:true})
-// }
-
 module.exports = {
     addIncome,
-    getIncome,
-    // deleteExpense
+    getSingleIncome,
 }
--- server/routes/income.js
+++ server/routes/income.js
@@ -2,7 +2,8 @@ const express = require("express")
 const router = express.Router()
 const {addIncome,getIncome} = require("../controller/income")
 
-router.route("/").post(addIncome).get(getIncome)
+router.route("/").post(addIncome)
+router.route("/:id").get(getIncome)
 // router.route("/:id").delete(deleteExpense)
 
 module.exports = router
