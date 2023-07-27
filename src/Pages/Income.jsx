c8320121c9f55dc45219e9cb0d022b19b572d216
Purav <puravshah20@gnu.ac.in>
2023-07-27 15:05:43 +0530

    Updated

--- src/Pages/Income.jsx
+++ src/Pages/Income.jsx
@@ -1,5 +1,18 @@
+import Sidebar from "../Components/Sidebar"
+import { Link } from "react-router-dom"
+import IncomeForm from "../Components/IncomeForm"
 const Income = () =>{
-
+    return(
+        <div>
+    <Sidebar />
+    <div className="p-4 sm:ml-64"><br></br>
+  <IncomeForm />
+    
+    
+    </div>
+</div>
+    
+    )
 }
 
 export default Income
