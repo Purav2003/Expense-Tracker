dfbaeaa001076c2f6c1c384d6068c167a537707e
Purav <puravshah20@gnu.ac.in>
2023-08-20 00:03:15 +0530

    Updated

--- src/Components/Navlinks.js
+++ src/Components/Navlinks.js
@@ -2,6 +2,7 @@ import * as dash from 'react-icons/rx'
 import * as income from 'react-icons/pi'
 import * as expenses from 'react-icons/fa'
 import * as report from 'react-icons/hi'
+import * as logout from 'react-icons/io'
 import * as profile from 'react-icons/cg'
 export const navLinks = [
   {
@@ -38,7 +39,12 @@ export const navLinks = [
     icon:<profile.CgProfile></profile.CgProfile>,
     link:'/profile',
     iconm:<profile.CgProfile className='text-white text-[25px]'></profile.CgProfile>,
-  }
+  },
+  {
+    id:"logout",
+    title:"Logout",
+    icon:<logout.IoIosLogOut></logout.IoIosLogOut>,
+  },
     
   ];
 
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,7 @@
 import Sidebar from "../Components/Sidebar"
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import dp from "../Assets/images/signup.png"
 const Profile = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -33,11 +33,29 @@ const Profile = () => {
 
     return (
         <div className="bg-white">
-            <div className="sm:ml-64"><br></br><br></br>
-                <center>{data.name}<br></br>
-                {data.mobile}<br></br>
-                {data.email}<br></br>
-                {data.createdAt.slice(0,10)}</center>
+            <br></br><br></br>
+
+            <div className="sm:ml-64">
+            <div className="lg:flex px-8 main-heading-mob">
+    <h1 className="text-4xl font-bold">Profile </h1> 
+    </div>
+                <br></br><br></br>
+            <center>
+                <div className="shadow-2xl w-[50%]">
+                <div>
+                    <img src={dp}  height={400} width={400} className="rounded-full"/>
+
+                </div><hr></hr><br></br>
+
+                    <table>
+                            <tr><td colSpan={4} className="font-semibold">Name:</td><td> {data.name}</td></tr>
+                            <tr><td colSpan={4} className="font-semibold">Email:</td><td> {data.email}</td></tr>
+                            <tr><td colSpan={4} className="font-semibold">Phone: </td><td>{data.mobile}</td></tr>
+                            <tr><td colSpan={4} className="font-semibold">Creation Date:</td><td> {data.createdAt?.slice(0,10)}</td></tr>
+                    </table><br></br>
+                </div>
+            </center>
+             
                 <Sidebar />
             </div>
         </div>
