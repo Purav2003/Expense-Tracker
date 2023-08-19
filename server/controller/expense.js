4307c1f2c294f4f383bf5e754b05bdccdfd2e6af
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-08-19 20:33:49 +0530

    implemented dynamic serch for expense module

--- server/controller/expense.js
+++ server/controller/expense.js
@@ -14,6 +14,7 @@ const getSingleExpense = async (req,res)=>{
     let result;
     let totalItems;
     const filterDate = req.query.daysAgo
+    const search = req.query.search
     let daysAgo = new Date()
     if(filterDate==="oneweek"){
         daysAgo.setDate(daysAgo.getDate() - 7)
@@ -35,6 +36,42 @@ const getSingleExpense = async (req,res)=>{
     } else{
         result = Expense.find({createdBy:id})
         totalItems = await Expense.find({createdBy:id}).countDocuments()
+    }
+    if(search){
+        result = Expense.find({
+            $and: [
+                {createdBy: id,},
+                {
+                    $or: [
+                        {description: {$regex: new RegExp(search, "i")}},
+                        {mode: {$regex: new RegExp(search, "i")}},
+                        {to: {$regex: new RegExp(search, "i")}},
+                        {category: {$regex: new RegExp(search, "i")}}
+                    ]
+                }   
+            ]
+        })
+        const result1 = await Expense.find({
+            $and: [
+                {createdBy: id,},
+                {
+                    $or: [
+                        {description: {$regex: new RegExp(search, "i")}},
+                        {mode: {$regex: new RegExp(search, "i")}},
+                        {to: {$regex: new RegExp(search, "i")}},
+                        {category: {$regex: new RegExp(search, "i")}}
+                    ]
+                }   
+            ]
+        })
+        const page = Number(req.query.page) || 1
+        const limit = Number(req.query.limit) || 5
+        const skip = (page - 1) * limit
+        result = result.skip(skip).limit(limit)
+        const expenses = await result.sort({ createdAt: -1 })
+        totalItems = result1.length
+        const totalPages = Math.ceil(totalItems / limit);
+        res.send({ totalItems, totalPages, expenses, currentPage: page, count: expenses.length, success: true, status: 200 })
     }
     if(!result){
         res.send({result,count:0,success:true,status:200})
