c6b05e2f1d3a32c1a687f3924f4bdd9fce645f23
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-08-19 17:38:03 +0530

    changes in pagination of income

--- server/controller/income.js
+++ server/controller/income.js
@@ -12,30 +12,30 @@ const addIncome = async (req,res)=>{
 const getSingleIncome = async (req,res)=>{
     const id = req.params.id
     let result;
-    let totalItems;
+    // let totalItems;
     const filterDate = req.query.daysAgo
     const search = req.query.search
     let daysAgo = new Date()
     if(filterDate==="oneweek"){
         daysAgo.setDate(daysAgo.getDate() - 7)
         result = Income.find({createdBy:id,date:{$gt:daysAgo}})
-        totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
+        // totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
     } else if(filterDate==="onemonth"){
         daysAgo.setMonth(daysAgo.getMonth() - 1)
         result = Income.find({createdBy:id,date:{$gt:daysAgo}})
-        totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
+        // totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
     } else if(filterDate==="sixmonth"){
         daysAgo.setMonth(daysAgo.getMonth() - 6)
         result = Income.find({createdBy:id,date:{$gt:daysAgo}})
-        totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
+        // totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
     } 
     else if(filterDate==="oneyear"){
         daysAgo.setFullYear(daysAgo.getFullYear() - 1)
         result = Income.find({createdBy:id,date:{$gt:daysAgo}})
-        totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
+        // totalItems = await Income.find({createdBy:id,date:{$gt:daysAgo}}).countDocuments()
     } else{
         result = Income.find({createdBy:id})
-        totalItems = await Income.find({createdBy:id}).countDocuments()
+        // totalItems = await Income.find({createdBy:id}).countDocuments()
     }
     if(search){
         result = Income.find({
@@ -44,12 +44,6 @@ const getSingleIncome = async (req,res)=>{
                 $regex: new RegExp(search,"i")
             }
         })
-        totalItems = await Income.find({
-            createdBy:id,
-            description:{
-                $regex: new RegExp(req.body.search,"i")
-            }
-        }).countDocuments()
     }
     if(!result){
         res.send({result,count:0,success:true,status:200})
@@ -57,10 +51,10 @@ const getSingleIncome = async (req,res)=>{
         const page = Number(req.query.page) || 1
         const limit = Number(req.query.limit) || 5
         const skip = (page-1)*limit
-        const totalPages = Math.ceil(totalItems / limit);
         result = result.skip(skip).limit(limit)
         const income = await result.sort({createdAt:-1})
-        res.send({totalItems,totalPages,income,currentPage:page,count:income.length,success:true,status:200})
+        const totalPages = Math.ceil(income.length/ limit);
+        res.send({totalItems:income.length,totalPages,income,currentPage:page,count:income.length,success:true,status:200})
     }
 }
 
