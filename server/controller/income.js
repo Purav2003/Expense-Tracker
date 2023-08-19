a3c63cc6ad172e96df0531c84dc84b8baca2ee7e
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-08-19 17:42:15 +0530

    testing income pagination

--- server/controller/income.js
+++ server/controller/income.js
@@ -57,10 +57,10 @@ const getSingleIncome = async (req,res)=>{
         const page = Number(req.query.page) || 1
         const limit = Number(req.query.limit) || 5
         const skip = (page-1)*limit
-        const totalPages = Math.ceil(result.length / limit);
+        const totalPages = Math.ceil(totalItems / limit);
         result = result.skip(skip).limit(limit)
         const income = await result.sort({createdAt:-1})
-        res.send({totalItems,totalPages,income,currentPage:page,count:income.length,success:true,status:200})
+        res.send({totalItems:income.length,totalPages,income,currentPage:page,count:income.length,success:true,status:200})
     }
 }
 
