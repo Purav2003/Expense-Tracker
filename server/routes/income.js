07433f5d4ab7bba99ebf0847c744c52dae4f6054
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-08-19 20:27:47 +0530

    removed search cotroller import from router of income

--- server/routes/income.js
+++ server/routes/income.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {addIncome,getSingleIncome,deleteIncome,search} = require("../controller/income")
+const {addIncome,getSingleIncome,deleteIncome} = require("../controller/income")
 
 router.route("/").post(addIncome)
 router.route("/:id").get(getSingleIncome).delete(deleteIncome)
