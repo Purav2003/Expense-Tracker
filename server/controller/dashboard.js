77438af72e92dab3f12a36af9b4d215d2a422be4
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-08-21 22:16:53 +0530

    sending individual incomes and expenses individually for dateData

--- server/controller/dashboard.js
+++ server/controller/dashboard.js
@@ -24,10 +24,7 @@ const dateData = async (req,res)=>{
     const date = new Date(req.body.date)
     const incomes = await Income.find({createdBy:id,date:date})
     const expenses = await Expense.find({createdBy:id,date:date})
-    const mixedData = [...incomes,...expenses]
-    mixedData.sort((a,b)=>a.date-b.date)
-    console.log({dateData:mixedData,success:true,status:200});
-    res.send({dateData:mixedData,success:true,status:200})
+    res.send({incomes,expenses,success:true,status:200})
 }
 
 module.exports = {
