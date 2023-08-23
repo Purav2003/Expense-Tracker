d2cc93f88eb083f2f7f6516f9109290ce7d222a8
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-08-23 23:21:38 +0530

    finally completed pagination of search of dashboard

--- server/controller/dashboard.js
+++ server/controller/dashboard.js
@@ -7,7 +7,7 @@ const transaction = async (req, res) => {
     const expenses = await Expense.find({ createdBy: id }).select("-category")
     const mixedData = [...incomes, ...expenses]
     mixedData.sort((a, b) => a.date - b.date)
-    res.send({ transaction: mixedData.slice(0, 5),success: true, status: 200 })
+    res.send({ transaction: mixedData.slice(0, 5), success: true, status: 200 })
 }
 
 const dateHighlight = async (req, res) => {
@@ -26,7 +26,7 @@ const dateData = async (req, res) => {
     const expenses = await Expense.find({ createdBy: id, date: date })
     const mixedData = [...incomes, ...expenses]
     mixedData.sort((a, b) => a.date - b.date)
-    res.send({ dateData: mixedData, success: true, status: 200 ,date:req.body.date})
+    res.send({ dateData: mixedData, success: true, status: 200, date: req.body.date })
 }
 
 const statistics = async (req, res) => {
@@ -79,17 +79,17 @@ const statistics = async (req, res) => {
     const totalMonthlyIncome = await Income.aggregate([
         {
             $match: {
-                createdBy:id,
-                $expr:{
-                    $and:[
-                        {$eq:[{$year:'$date'},currentYear]},
-                        {$eq:[{$month:'$date'},currentMonth]} 
+                createdBy: id,
+                $expr: {
+                    $and: [
+                        { $eq: [{ $year: '$date' }, currentYear] },
+                        { $eq: [{ $month: '$date' }, currentMonth] }
                     ]
                 }
             }
         },
         {
-            $group:{
+            $group: {
                 _id: null,
                 totalAmount: {
                     $sum: '$amount'
@@ -106,17 +106,17 @@ const statistics = async (req, res) => {
     const totalMonthlyExpense = await Expense.aggregate([
         {
             $match: {
-                createdBy:id,
-                $expr:{
-                    $and:[
-                        {$eq:[{$year:'$date'},currentYear]},
-                        {$eq:[{$month:'$date'},currentMonth]} 
+                createdBy: id,
+                $expr: {
+                    $and: [
+                        { $eq: [{ $year: '$date' }, currentYear] },
+                        { $eq: [{ $month: '$date' }, currentMonth] }
                     ]
                 }
             }
         },
         {
-            $group:{
+            $group: {
                 _id: null,
                 totalAmount: {
                     $sum: '$amount'
@@ -133,74 +133,60 @@ const statistics = async (req, res) => {
     const incomes = await Income.find({ createdBy: id })
     const expenses = await Expense.find({ createdBy: id })
     const totalTransactions = [...incomes, ...expenses].length
-    res.send({ totalIncome:totalIncome[0].totalAmount, totalExpense:totalExpense[0].totalAmount, totalMonthlyIncome:totalMonthlyIncome[0].totalAmount, totalMonthlyExpense:totalMonthlyExpense[0].totalAmount ,totalTransactions})
+    res.send({ totalIncome: totalIncome[0].totalAmount, totalExpense: totalExpense[0].totalAmount, totalMonthlyIncome: totalMonthlyIncome[0].totalAmount, totalMonthlyExpense: totalMonthlyExpense[0].totalAmount, totalTransactions })
 }
 
-const search = async(req,res)=>{
+const search = async (req, res) => {
     const id = req.params.id
     const search = req.query.search
-    let resultIncome = Income.find({
-        $and: [
-            {createdBy: id,},
-            {
-                $or: [
-                    {description: {$regex: new RegExp(search, "i")}},
-                    {mode: {$regex: new RegExp(search, "i")}},
-                    {from: {$regex: new RegExp(search, "i")}},
-                ]
-            }   
-        ]
-    })
     const incomeItems = await Income.find({
         $and: [
-            {createdBy: id,},
+            { createdBy: id, },
             {
                 $or: [
-                    {description: {$regex: new RegExp(search, "i")}},
-                    {mode: {$regex: new RegExp(search, "i")}},
-                    {from: {$regex: new RegExp(search, "i")}},
+                    { description: { $regex: new RegExp(search, "i") } },
+                    { mode: { $regex: new RegExp(search, "i") } },
+                    { from: { $regex: new RegExp(search, "i") } },
                 ]
-            }   
-        ]
-    })
-    let resultExpense = Expense.find({
-        $and: [
-            {createdBy: id,},
-            {
-                $or: [
-                    {description: {$regex: new RegExp(search, "i")}},
-                    {mode: {$regex: new RegExp(search, "i")}},
-                    {to: {$regex: new RegExp(search, "i")}},
-                    {category: {$regex: new RegExp(search, "i")}}
-                ]
-            }   
+            }
         ]
     })
     const expenseItems = await Expense.find({
         $and: [
-            {createdBy: id,},
+            { createdBy: id, },
             {
                 $or: [
-                    {description: {$regex: new RegExp(search, "i")}},
-                    {mode: {$regex: new RegExp(search, "i")}},
-                    {to: {$regex: new RegExp(search, "i")}},
-                    {category: {$regex: new RegExp(search, "i")}}
+                    { description: { $regex: new RegExp(search, "i") } },
+                    { mode: { $regex: new RegExp(search, "i") } },
+                    { to: { $regex: new RegExp(search, "i") } },
+                    { category: { $regex: new RegExp(search, "i") } }
                 ]
-            }   
+            }
         ]
     })
+    const mixedData = [...incomeItems, ...expenseItems]
+    mixedData.sort((a, b) => a.date - b.date)
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 5
-    const skip = (page - 1) * limit
-    resultIncome = resultIncome.skip(skip).limit(limit)
-    const finalResultIncome = await resultIncome.sort({ createdAt: -1 })
-    resultExpense = resultExpense.skip(skip).limit(limit)
-    const finalResultExpense = await resultExpense.sort({createdAt:-1})
-    const totalItems = incomeItems.length + expenseItems.length
-    const totalPages = Math.ceil(totalItems / limit);
-    const mixedData = [...finalResultIncome, ...finalResultExpense]
-    mixedData.sort((a, b) => a.date - b.date)
-    res.send({totalItems, totalPages,currentPage: page,searchedData:mixedData,success:true,status:200})
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const searchedData = {}
+    if (startIndex > 0) {
+        searchedData.previous = {
+            page: page - 1,
+            limit: limit
+        };
+    }
+    if (endIndex < mixedData.length) {
+        searchedData.next = {
+            page: page + 1,
+            limit: limit
+        };
+    }
+    searchedData.results = mixedData.slice(startIndex, endIndex);
+    const totalItems = mixedData.length
+    const totalPages = Math.ceil(totalItems / limit)
+    res.send({ totalItems, totalPages, currentPage: page, searchedData, success: true, status: 200 })
 }
 
 module.exports = {
