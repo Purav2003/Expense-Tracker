68f60886f368b60ae3f47d74ef58bf346ec08aba
Purav <puravshah20@gnu.ac.in>
2023-08-26 23:53:44 +0530

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import open from 'open';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -6,4 +7,19 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  server: {
+    // Port that Vite will use (you can change this to your desired port)
+    port: 3000,
+    // Hook to run after the server starts
+    hmr: {
+      // This function will run after the server starts
+      after() {
+        // Define the URL or file path you want to open
+        const urlToOpen = 'http://localhost:3000'; // Change this to your desired URL
+
+        // Open the URL in the default browser
+        open(urlToOpen);
+      },
+    },
+  },
 });
