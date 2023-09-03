6ecb1e307247661a526c088de3ef915d33dedba6
Purav <puravshah20@gnu.ac.in>
2023-09-03 12:12:09 +0530

    Updated Landing Page

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import '../assets/css/landing.css'
 import heroImage from '../assets/images/login.png'
-
+import landing1 from '../assets/images/landing-1.png'
+import landing2 from '../assets/images/landing-2.png'
+import landing3 from '../assets/images/landing-3.png'
+import landing4 from '../assets/images/landing-4.png'
+import {useState} from 'react'
+import { useEffect } from 'react';
 
 const LandingPage = () => {
-
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+  
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [prevScrollPos]);
+  
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const visible = prevScrollPos > currentScrollPos || currentScrollPos < 10;
+      setVisible(visible);
+      setPrevScrollPos(currentScrollPos);
+    };
     return (
         <div className='landing'>
-            <header>
-                <nav>
-                    <div className="logo">Expense Tracker</div>
-                    {/* <ul>
-                        <li><a href="#">Dashboard</a></li>
-                        <li><a href="#">Income</a></li>
-                        <li><a href="#">Expense</a></li>
-                        <li><a href="#">Reports</a></li>
-                    </ul> */}
-                </nav>
-            </header>
+                  <header className={`fixed w-full z-[100] ${visible ? '' : 'navbar-hidden'}`}>
+        <nav>
+          <div className="logo">Expense Tracker</div>
+        </nav>
+      </header><br></br><br></br>
 
             <section className="hero flex w-full justify-center items-center">
                 <div className="hero-content text-center">
@@ -25,13 +39,36 @@ const LandingPage = () => {
                     <p>Simplify Your Expenses with Expense Tracker</p>
                     <a href="#" className="cta-button">Get Started</a>
                 </div>
-                <div className='text-center'><img src={heroImage} alt="Hero" /></div>
 
             </section>
 
             <section className="features">
-                <h2>Key Features</h2>
+                <h2 className='uppercase'>Features</h2>
+                <p>Organize your incomes, keep track of expenses while traveling and understand where all your money are with some great, yet simple features.</p>
+                <br></br><br></br>
+                <div className='px-24'>
+                    <div className="lg:grid lg:grid-cols-4 gap-4">
+                        {/* 1st Row */}
+                        <div className="lg:col-span-1 flex flex-col items-center justify-center text-center">
+                            <img src={landing1} alt="Image 1" className='p-8 img-1 custom-border' />
+                            <h1 className='pt-4 text-black font-semibold text-[18px]'> INTUITIVE & CLEAN</h1><hr className='text-[#e96656] bg-[#e96656] mt-2 pt-[2px] w-[120px]'></hr>
+                            <p className='h-[18vh]'><br></br>Intuitive, clean and simple way of adding daily expenses and incomes. main screen calendar that gives a quick monthly overview over all transactions</p>
+                        </div>
+                        <div className="lg:col-span-1 flex flex-col items-center justify-center text-center">
+                            <img src={landing2} alt="Image 2" className='p-8 img-2 custom-border' />
+                            <h1 className='pt-4 text-black font-semibold text-[18px]'> SECURE</h1><hr className='text-center justify-center items-center text-[#34d293] bg-[#34d293] mt-2 pt-[2px] w-[40px]'></hr>
+                            <p className='h-[18vh]'><br></br>Your data is always safe wherever your are and what ever device you are using.</p>                        </div>
+                        <div className="lg:col-span-1 flex flex-col items-center justify-center text-center">
+                            <img src={landing3} alt="Image 3" className='p-8 img-3 custom-border' />
+                            <h1 className='pt-4 text-black font-semibold text-[18px]'> COMPREHENSIVE</h1><hr className='text-[#3ab0e2] bg-[#3ab0e2] mt-2 pt-[2px] w-[90px]'></hr>
+                            <p><br></br>A compact yet comprehensive list of expense and income categories. Add or edit categories, if defaults don't work for you</p>                        </div>
+                        <div className="lg:col-span-1 flex flex-col items-center justify-center text-center">
+                            <img src={landing4} alt="Image 4" className='p-8 img-4 custom-border' />
+                            <h1 className='pt-4 text-black font-semibold text-[18px]'> RELIABLE & USEFUL</h1><hr className='text-[#f7d861] bg-[#f7d861] mt-2 pt-[2px] w-[120px]'></hr>
+                            <p><br></br>Set reminders for future inflow or outflow transactions and recurrent payments - never miss another deadline</p>                        </div>
+                    </div>
 
+                </div><br></br><br></br>
                 <div className="feature flex w-full justify-center items-center">
                     <div className='text-center'>
                         <img src={heroImage} alt="Expense" />
@@ -61,14 +98,14 @@ const LandingPage = () => {
                 <div className="feature flex w-full justify-center items-center">
                     <div className='text-center w-[50%]'><h3>View your data </h3>
                         <p>View income / expense as you want</p></div>
-                        <div className='text-center'>
+                    <div className='text-center'>
                         <img src={heroImage} alt="Expense" />
                     </div>
                 </div>
                 <div className="feature flex w-full justify-center items-center">
-                <div className='text-center'>
+                    <div className='text-center'>
                         <img src={heroImage} alt="Expense" />
-                    </div>                    
+                    </div>
                     <div className='text-center w-[50%]'><h3>Report </h3>
                         <p>View / Download your reports effortlessly</p></div>
                 </div>
